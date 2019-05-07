bbeba9b4393de0fef5393df04f7c0f0eef04870c
kidnapper1995 <kidnapper1995@163.com>
2019-05-07 15:41:59 +0800

    Basic

---
+++ src/main/java/com/example/Study/dao/SUserDao.java
@@ -0,0 +1,29 @@
+package com.example.Study.dao;
+
+import com.example.Study.bean.SUser;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+
+/**
+ * 用户信息查询
+ * @author Gu
+ */
+@Mapper
+public interface SUserDao {
+	/**
+	 * 根据用户名获取用户
+	 *
+	 * @param name
+	 * @return
+	 */
+	@Select(value = " SELECT su.* FROM s_user su WHERE su.name = #{name} ")
+	public SUser findSUserByName(String name);
+
+
+	@Insert(value=" INSERT s_user (name,realName,password) values (#{userName},#{realName},#{password})")
+	public boolean addUser(String userName, String realName, String password);
+
+
+}
